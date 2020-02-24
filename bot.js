a6dd6cd5fee2caf474bf9d6e412966d18f5da655
henek <eagluhih@gmail.com>
2020-02-25 02:09:01 +0500

    Добавлено завершение работы при ошибке

--- bot.js
+++ bot.js
@@ -14,34 +14,36 @@ const purchasedItems = [
   const profiles = await t.getProfiles();
   const profile = profiles.find((p) => p.savage);
   await t.selectProfile(profile);
-  const roubles = t.profile.getRoubles();
   console.log(`> Привет ${profile.Info.Nickname}`);
-  // console.log(`> Начальный баланс: ${t.profile.getRoubles()}`);
 
   await t.getI18n('ru');
 
-  await setInterval(async () => {
-    // eslint-disable-next-line no-restricted-syntax
-    for (const purchasedItem of purchasedItems) {
-      // eslint-disable-next-line no-await-in-loop
-      const search = await t.searchMarket(0, 15, {
-        currency: 1,
-        priceTo: purchasedItem.max_price,
-        handbookId: purchasedItem.id,
-      });
-
-      if (search.offers.length) {
-        const offer = search.offers[0];
+  const timer = await setInterval(async () => {
+    try {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const purchasedItem of purchasedItems) {
         // eslint-disable-next-line no-await-in-loop
-        const response = await offer.buyWithRoubles(offer.items.length);
-        console.log(`> Найдена ${purchasedItem.name}: ${offer.requirementsCost}`);
+        const search = await t.searchMarket(0, 15, {
+          currency: 1,
+          priceTo: purchasedItem.max_price,
+          handbookId: purchasedItem.id,
+        });
 
-        if (response.items.new) {
-          console.log(`> Куплено: ${purchasedItem.name}. ${offer.items.length} шт.`);
-        } else {
-          console.log(`> Ошибка: ${response.badRequest[0].errmsg}`);
+        if (search.offers.length) {
+          const offer = search.offers[0];
+          // eslint-disable-next-line no-await-in-loop
+          const response = await offer.buyWithRoubles(offer.items.length);
+          console.log(`> Найдена ${purchasedItem.name}: ${offer.requirementsCost}`);
+
+          if (response.items.new) {
+            console.log(`> Куплено: ${purchasedItem.name}. ${offer.items.length} шт.`);
+          } else {
+            console.log(`> Ошибка: ${response.badRequest[0].errmsg}`);
+          }
         }
       }
+    } catch (e) {
+      clearInterval(timer);
     }
   }, 1000);
 })();
